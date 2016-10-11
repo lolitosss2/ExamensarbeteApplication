2290b3d0349eac4d1ed4e6531e6e887f8e42caf0
lolitosss2 <lolita-mage@mail.ru>
2016-10-11 13:48:03 +0200

    Got your changes, made some changes

--- app/src/main/java/db/hfad/com/healthapplication/MainActivity.java
+++ app/src/main/java/db/hfad/com/healthapplication/MainActivity.java
@@ -4,13 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-<<<<<<< HEAD
+
     //test
-=======
+
+    //Lolita testar
     // Testar
     // Hanna testar lite
     String test;
->>>>>>> refs/remotes/origin/Hanna
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
