fcf60706dcbdcaa798a991ab8aaf10ed1e3ddbc4
lolitosss2 <lolita-mage@mail.ru>
2016-10-11 12:58:42 +0200

    Test

--- app/src/main/java/db/hfad/com/healthapplication/MainActivity.java
+++ app/src/main/java/db/hfad/com/healthapplication/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //test
+    // Testar
+    String test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
