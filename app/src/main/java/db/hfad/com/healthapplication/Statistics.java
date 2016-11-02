daee1998005ce3fb1ac0efa436f6ab11b711add5
lolitosss2 <lolita-mage@mail.ru>
2016-11-02 23:50:16 +0100

    hej

---
+++ app/src/main/java/db/hfad/com/healthapplication/Statistics.java
@@ -0,0 +1,16 @@
+package db.hfad.com.healthapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Lolita on 2016-11-02.
+ */
+public class Statistics extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
+}
