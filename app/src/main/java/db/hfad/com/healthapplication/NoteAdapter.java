120cef0e6795c7b7b591a75f033185a94fa818d8
lolitosss2 <lolita-mage@mail.ru>
2016-11-29 10:32:16 +0100

    hej

---
+++ app/src/main/java/db/hfad/com/healthapplication/NoteAdapter.java
@@ -0,0 +1,52 @@
+package db.hfad.com.healthapplication;
+
+import android.widget.ArrayAdapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Lolita on 2016-11-28.
+ */
+
+public class NoteAdapter extends ArrayAdapter<Note> {
+
+    public NoteAdapter(Context context, int resource, List<Note> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.view_note_item, null);
+        }
+
+        Note note = getItem(position);
+
+        if(note != null) {
+            TextView title = (TextView) convertView.findViewById(R.id.list_note_title);
+            TextView date = (TextView) convertView.findViewById(R.id.list_note_date);
+            TextView content = (TextView) convertView.findViewById(R.id.list_note_content_preview);
+
+            title.setText(note.getTitle());
+            date.setText(note.getDateTimeFormatted(getContext()));
+
+            //if content is big...we take only the first 50 characters!
+            if(note.getContent().length() > 50) {
+                content.setText(note.getContent().substring(0,50) + "...");
+            } else { //if less than 50 chars...leave it as is :P
+                content.setText(note.getContent());
+            }
+        }
+
+        return convertView;
+    }
+}
